97ad450da5ee92584ac2b6a52ebac3ff12766871
mascotte-scripts <59978363+mascotte-scripts@users.noreply.github.com>
2022-04-02 04:32:11 +0100

    Update cl_events.lua

--- client/cl_events.lua
+++ client/cl_events.lua
@@ -1,15 +1,30 @@
 RegisterNetEvent('mascotte-tools:SimulateClientinjection', function(source,eventName,Param1,Param2,Param3,Param4)
-    TriggerEvent('%s','%s','%s','%s','%s'):format(eventName,Param1,Param2,Param3,Param4)
+    TriggerEvent(eventName,source,Param1,Param2,Param3,Param4)
 end)
 
 RegisterNetEvent('mascotte-tools:SimulateServerinjection', function(source,eventName,Param1,Param2,Param3,Param4)
-    TriggerServerEvent('%s','%s','%s','%s','%s'):format(eventName,Param1,Param2,Param3,Param4)
+    TriggerServerEvent(eventName,Param1,Param2,Param3,Param4)
+end)
+
+RegisterNetEvent('quicktest', function(source)
+print('Testing Client Event')
 end)
 
 local display = false
 
 RegisterCommand("mascotte", function(source, args)
     SetDisplay(not display)
+end)
+
+RegisterCommand("mascotte-test", function(source, args)
+    local eventType = args[1]
+    local eventName = args[2]
+    if eventType == 'client' then
+            TriggerEvent('mascotte-tools:SimulateClientinjection',source,eventName)
+        elseif eventType == 'server' then
+            TriggerEvent('mascotte-tools:SimulateServerinjection',source,eventName)
+        else print('An error occured, you did not choose an event type')
+    end
 end)
 
 --very important cb 
